5cd08171dc94c4fb800ae40ae947ae0bbd51ef85
caldavi3227 <davidsoncaleb97@gmail.com>
2018-06-14 20:20:11 -0700

    Operational server/Header start

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const React = require('react');
+const mongoDB = require('mongodb');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('PORT', 3227);
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(app.get('PORT'), function() {
+  console.log('Express server listening on PORT ' + app.get('PORT'));
+});
+
+module.exports = app;
